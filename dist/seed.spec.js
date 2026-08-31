8f5ea80983ac783333f2544ba593f715d1494324
NikIBM <Nithin.Kumar.K@ibm.com>
2026-08-31 12:37:52 +0530

    Cleanup

--- dist/seed.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const test_1 = require("@playwright/test");
-test_1.test.describe('Test group', () => {
-    (0, test_1.test)('seed', async ({ page }) => {
-        // generate code here.
-    });
-});
